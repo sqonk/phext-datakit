91248de21181f558c2090c075faea9676d79a9b6
Theo Howell <sqonk@sqonk.com.au>
2021-01-13 11:55:28 +1100

    Enhanced SMA class.
    Added ArrayAccess, Countable Iterator interfaces. Ability to iterate over all previously calculated averages. Ability to add multiple values in one call. Get all calculated averages.

--- src/SMA.class.php
+++ src/SMA.class.php
@@ -21,45 +21,128 @@ namespace sqonk\phext\datakit;
 /**
  * A simple class for management of a Simple Moving Average. It works by alternating between
  * adding new values to the array and calculating the current average.
+ * 
+ * SMA adheres to various array-like behaviour protocols. You should keep in 
+ * mind that whenever you access values from the class you will be receiving the relevant
+ * average, not the original raw value you placed in previously.
  */
-class SMA
+class SMA implements \ArrayAccess, \Countable, \IteratorAggregate
 {
 	protected $values;
+    protected $averages;
+    
+    // =====================
+    // = Interface methods =
+    // =====================
+    
+    /**
+     * Returns the total number of averages calculated so far.
+     */
+    public function count(): int
+    {
+        return $this->averages->count();
+    }
+    
+	public function getIterator(): \ArrayIterator
+	{
+        $array = $this->defaultPrecision ? $this->averages->round($this->defaultPrecision)->array() : 
+            $this->averages->array();
+		return new \ArrayIterator($array);
+	}
+    
+	public function offsetSet($index, $value)
+	{
+		if ($index === null)
+			$this->add($value);
+		else
+			throw new \Exception("Existing values of an SMA can not be overwritten.");
+	}
+	
+	public function offsetGet($index)
+	{
+		$value = $this->averages->get($index);
+        if (is_int($this->defaultPrecision))
+            $value = round($value, $this->defaultPrecision);
+        
+        return $value;
+	}
+	
+	public function offsetExists($index)
+	{
+		return array_key_exists($index, $this->averages->array());
+	}
+	
+	public function offsetUnset($index)
+	{
+		throw new \Exception("Existing values can not be removed from an SMA.");
+	}
+    
+    
+    // ======================
+    // = Main class methods =
+    // ======================
 	
     /**
      * Construct a new SMA with the specified maximum number of values.
      */
-	public function __construct(int $maxItems)
+	public function __construct(int $maxItems, ?int $defaultPrecision = null)
 	{
         if ($maxItems < 1)
             throw new \InvalidArgumentException("maxItems must be a number greater or equal to 1");
+        
 		$this->values = vector()->constrain($maxItems);
+        $this->averages = vector();
+        
+        $this->defaultPrecision = $defaultPrecision;
 	}
 	
     /**
      * Add a new value to the SMA. The value must be numerical in nature.
      */
-	public function add($value)
+	public function add(...$values): SMA
 	{
-		if (! is_numeric($value))
-			throw new \InvalidArgumentException("Only numeric values are accepted. [$value] was provided.");
+        foreach ($values as $value)
+        {
+    		if (! is_numeric($value))
+    			throw new \InvalidArgumentException("Only numeric values are accepted. [$value] was provided.");
 		
-		$this->values->add($value);
+    		$this->values->add($value);
+            $this->averages->add($this->values->avg());
+        }
 		
 		return $this;
 	}
 	
     /**
-     * Return the calculated result of the SMA as it currently stands. You can optionally pass in a value to
-     * $precision to control the amount of decimal places that the result is rounded to.
+     * Return the calculated result of the SMA as it currently stands, optionally rounding it to the specified
+     * precision. If $precision is NULL then it falls back to the default precision specified at the time 
+     * of object creation.
      */
-	public function result(?int $precision = null)
+	public function result(?int $precision = null): float
 	{
-		$avg = $this->values->avg();
+        $precision ??= $this->defaultPrecision;
+        
+		$avg = $this->averages->last();
 		return ($precision !== null) ? round($avg, $precision) : $avg;
 	}
+    
+    /**
+     * Return all acquired averages, optionally rounding them to the specified precision. If $precision is
+     * NULL then it falls back to the default precision specified at the time of object creation.
+     */
+    public function all(?int $precision = null): array
+    {
+        $precision ??= $this->defaultPrecision;
+        
+        if ($precision) {
+            $copy = clone $this->averages;
+            return $copy->round($precision)->array();
+        }
+        
+        return $this->averages->array();
+    }
 	
-	public function __toString()
+	public function __toString(): string
 	{
 		return "SMA: ".$this->result();
 	}
--- tests/SMATest.php
+++ tests/SMATest.php
@@ -33,6 +33,38 @@ class SMATest extends TestCase
         }
     }
     
+    public function testCount()
+    {
+        $sma = new SMA(5);
+        $expected = [1.0,1.5,2.0,2.5,3.0,4.0,5.0,6.0,7.0,8.0];
+        foreach (range(1, 10) as $i) {
+            $sma->add($i);
+        }
+        
+        $this->assertSame(10, count($sma));
+    }
+    
+    public function testAcquiredAverages()
+    {
+        $sma = new SMA(5);
+        $expected = [1.0,1.5,2.0,2.5,3.0,4.0,5.0,6.0,7.0,8.0];
+        foreach (range(1, 10) as $i) {
+            $sma->add($i);
+        }
+        
+        foreach ($sma as $i => $avg)
+            $this->assertEquals($expected[$i], $avg);
+    }
+    
+    public function testAddMultiWithReturnAllAndRounding()
+    {
+        $sma = new SMA(3, 2);
+        $sma->add(1,0.43,3,4.33,5,6,7);
+
+        $this->assertSame([1.0,0.72,1.48,2.59,4.11,5.11,6.0], $sma->all());
+        $this->assertSame([1.0,0.715,1.477,2.587,4.11,5.11,6.0], $sma->all(3));
+    }
+    
     public function testInvalidMax()
     {
         $this->expectException(InvalidArgumentException::class);
@@ -44,5 +76,36 @@ class SMATest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         $ema = new SMA(3);
         $ema->add('token');
+    }
+    
+    public function testExceptionForAttemptedRemoval()
+    {
+        $this->expectException(Exception::class);
+        $sma = new SMA(3);
+        $sma->add(1,2,3);
+        unset($sma[1]);
+    }
+    
+    public function testExceptionForAttemptedSet()
+    {
+        $this->expectException(Exception::class);
+        $sma = new SMA(3);
+        $sma->add(1,2,3);
+        $sma[1] = 4;
+    }
+    
+    public function testGetViaArrayAccess()
+    {
+        $sma = new SMA(3);
+        $sma->add(1,2,3);
+        $this->assertSame(1.5, $sma[1]);
+    }
+    
+    public function testAddViaArrayAccess()
+    {
+        $sma = new SMA(3);
+        $sma->add(1,2,3);
+        $sma[] = 4;
+        $this->assertSame(2, $sma[2]);
     }
 }
