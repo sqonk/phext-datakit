68445745ad9ca5d6c2440001834f039d54588c3e
Theo Howell <sqonk@sqonk.com.au>
2021-01-13 21:07:58 +1100

    Extra functionality from SMA added into EMA.

--- src/EMA.class.php
+++ src/EMA.class.php
@@ -22,64 +22,139 @@ namespace sqonk\phext\datakit;
  * A simple class for management of a Exponential Moving Average. It works by alternating between
  * adding new values to the array and calculating the current average.
  */
-class EMA
+class EMA implements \ArrayAccess, \Countable, \IteratorAggregate
 {
 	protected $limit;
 	protected $values;
+    protected $averages;
 	protected $previous = 0;
-	protected $latest = 0;
-	
+    
+    // =====================
+    // = Interface methods =
+    // =====================
+    
     /**
-     * Construct a new EMA with the specified maximum number of values.
+     * Returns the total number of averages calculated so far.
      */
-	public function __construct(int $maxItems)
+    public function count(): int
+    {
+        return $this->averages->count();
+    }
+    
+	public function getIterator(): \ArrayIterator
 	{
-        if ($maxItems < 1)
-            throw new \InvalidArgumentException("maxItems must be a number greater or equal to 1");
-		$this->values = vector()->constrain($maxItems);
-		$this->limit = $maxItems;
+        $array = $this->defaultPrecision ? $this->averages->round($this->defaultPrecision)->array() : 
+            $this->averages->array();
+		return new \ArrayIterator($array);
+	}
+    
+	public function offsetSet($index, $value)
+	{
+		if ($index === null)
+			$this->add($value);
+		else
+			throw new \Exception("Existing values of an SMA can not be overwritten.");
+	}
+	
+	public function offsetGet($index)
+	{
+		$value = $this->averages->get($index);
+        if (is_int($this->defaultPrecision))
+            $value = round($value, $this->defaultPrecision);
+        
+        return $value;
+	}
+	
+	public function offsetExists($index)
+	{
+		return array_key_exists($index, $this->averages->array());
+	}
+	
+	public function offsetUnset($index)
+	{
+		throw new \Exception("Existing values can not be removed from an SMA.");
 	}
 	
     /**
-     * Add a new value to the EMA. The value must be numerical in nature.
+     * Construct a new EMA with the specified maximum number of values.
+     * 
+     * -- parameters:
+     * @param $maxItems The maximum amount of values that the moving average is allowed to work off of. As new values are added onto the end, old values are moved off the front.
+     * @param $defaultPrecision If set, will automatically round all averages to the given decimal precision.
      */
-	public function add($value)
+	public function __construct(int $maxItems, ?int $defaultPrecision = null)
 	{
-		if (! is_numeric($value))
-			throw new \InvalidArgumentException("Only numeric values are accepted. [$value] was provided.");
+        if ($maxItems < 1)
+            throw new \InvalidArgumentException("maxItems must be a number greater or equal to 1");
+        
+		$this->values = vector()->constrain($maxItems);
+		$this->limit = $maxItems;
+        
+        $this->averages = vector();
+        
+        $this->defaultPrecision = $defaultPrecision;
+	}
+	
+    /**
+     * Add one or more new values to the EMA. The value must be numerical in nature.
+     */
+	public function add(...$values): EMA
+	{
+        foreach ($values as $value)
+        {
+    		if (! is_numeric($value))
+    			throw new \InvalidArgumentException("Only numeric values are accepted. [$value] was provided.");
 		
-		$this->values->add($value);
-		
-		$this->calc($value); // update the moving average based on the new set of values.
+    		$this->values->add($value);		
+    		$this->averages->add($this->calc($value)); 
+        }
 		
 		return $this;
 	}
 	
-	protected function calc($newValue)
+	protected function calc($newValue): float
 	{
 		$count = count($this->values);
 		if ($this->values->count() == 1)
-			$this->latest = $this->values->first();
+			return $this->values->first();
+
+		$this->previous = $this->averages->last();
 		
-		else
-		{
-			$this->previous = $this->latest;
-			
-			$k = 2 / ($this->limit + 1);
-			$this->latest = ($k * $newValue) + ((1 - $k) * $this->previous);
-		}
+		$k = 2 / ($this->limit + 1);
+		return ($k * $newValue) + ((1 - $k) * $this->previous);
 	}
 	
     /**
      * Return the calculated result of the EMA as it currently stands. You can optionally pass in a value to
-     * $precision to control the amount of decimal places that the result is rounded to.
+     * $precision to control the amount of decimal places that the result is rounded to. If $precision is NULL then it falls back to the default precision specified at the time of object creation.
      */
-	public function result(?int $precision = null)
+	public function result(?int $precision = null): float
 	{
-		return ($precision !== null) ? round($this->latest, $precision) : $this->latest;
+        if ($precision === null)
+            $precision = $this->defaultPrecision;
+        
+		$avg = $this->averages->last();
+		return ($precision !== null) ? round($avg, $precision) : $avg;
 	}
+    
+    /**
+     * Return all acquired averages, optionally rounding them to the specified precision. If $precision is
+     * NULL then it falls back to the default precision specified at the time of object creation.
+     */
+    public function all(?int $precision = null): array
+    {
+        if ($precision === null)
+            $precision = $this->defaultPrecision;
+        
+        if ($precision) {
+            $copy = clone $this->averages;
+            return $copy->round($precision)->array();
+        }
+        
+        return $this->averages->array();
+    }
 	
-	public function __toString()
+	public function __toString(): string
 	{
 		return "EMA: {$this->latest}";
 	}
--- tests/EMATest.php
+++ tests/EMATest.php
@@ -33,6 +33,69 @@ class EMATest extends TestCase
         }
     }
     
+    public function testCount()
+    {
+        $ema = new EMA(5);
+        foreach (range(1, 10) as $i) {
+            $ema->add($i);
+        }
+        
+        $this->assertSame(10, count($ema));
+    }
+    
+    public function testAcquiredAverages()
+    {
+        $ema = new EMA(5, 2);
+        $expected = [1.00,1.33,1.89,2.59,3.4,4.26,5.18,6.12,7.08,8.05];
+        foreach (range(1, 10) as $i) {
+            $ema->add($i);
+        }
+        
+        foreach ($ema as $i => $avg)
+            $this->assertEquals($expected[$i], $avg);
+    }
+    
+    public function testAddMultiWithReturnAllAndRounding()
+    {
+        $ema = new EMA(3, 2);
+        $ema->add(1,0.43,3,4.33,5,6,7);
+
+        $this->assertSame([1.0,0.72,1.86,3.09,4.05,5.02,6.01], $ema->all());
+        $this->assertSame([1.0,0.715,1.858,3.094,4.047,5.023,6.012], $ema->all(3));
+    }
+
+    
+    public function testExceptionForAttemptedRemoval()
+    {
+        $this->expectException(Exception::class);
+        $ema = new EMA(3);
+        $ema->add(1,2,3);
+        unset($ema[1]);
+    }
+    
+    public function testExceptionForAttemptedSet()
+    {
+        $this->expectException(Exception::class);
+        $ema = new EMA(3);
+        $ema->add(1,2,3);
+        $ema[1] = 4;
+    }
+    
+    public function testGetViaArrayAccess()
+    {
+        $ema = new EMA(3, 1);
+        $ema->add(1,2,3);
+        $this->assertSame(1.5, $ema[1]);
+    }
+    
+    public function testAddViaArrayAccess()
+    {
+        $ema = new EMA(3, 2);
+        $ema->add(1,2,3);
+        $ema[] = 4;
+        $this->assertSame(2.25, $ema[2]);
+    }
+    
     public function testInvalidMax()
     {
         $this->expectException(InvalidArgumentException::class);
